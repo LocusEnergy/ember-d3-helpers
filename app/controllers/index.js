cbfb94cfc1c4ee25ae1bf1d3f20a2ae74beab2a7
Taras Mankovski <tarasm@gmail.com>
2016-03-10 10:08:03 -0500

    Fixed wrong item being removed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
+let id = 0;
+
 export default Ember.Controller.extend({
   data: [
-    { r: 10, x: 10, y: 10 },
-    { r: 20, x: 30, y: 30 },
-    { r: 30, x: 65, y: 65 }
+    { r: 10, x: 10, y: 10, id: ++id },
+    { r: 20, x: 30, y: 30, id: ++id },
+    { r: 30, x: 65, y: 65, id: ++id }
   ],
   radius: 30,
   x: 120,
@@ -18,12 +20,16 @@ export default Ember.Controller.extend({
   getYCoord({y}) {
     return y;
   },
+  getId({id}) {
+    return id;
+  },
   actions: {
     addCircle(r) {
       let data = this.get('data');
       let x = parseInt(this.get('x'), 10);
       let y = this.get('y');
-      data.pushObject({ r, x, y});
+      ++id;
+      data.pushObject({ r, x, y, id });
       this.setProperties({
         x: x + 2 * r,
         radius: r - 5
