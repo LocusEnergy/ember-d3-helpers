cd0ed78cd7f49093e410b3c6930f3c2711b8c86a
Taras Mankovski <tarasm@gmail.com>
2016-07-30 02:40:28 +0530

    d3-graph can now be nested

--- addon/components/d3-graph.js
+++ addon/components/d3-graph.js
@@ -2,12 +2,42 @@ import Ember from 'ember';
 import { select } from 'd3-selection';
 import layout from '../templates/components/d3-graph';
 
+const {
+  isNone,
+  typeOf,
+  defineProperty,
+  computed: { oneWay }
+} = Ember;
+
 export default Ember.Component.extend({
   layout,
   tagName: 'g',
+
+  init() {
+    this._super(...arguments);
+    let { _selection, _graph } = this.getProperties(['_selection', '_graph']);
+
+    let isNested = _selection && _graph;
+    if (isNested) {
+      defineProperty(this, 'selection', oneWay('_selection'));
+      defineProperty(this, 'graph', oneWay('_graph'));
+      this.set('tagName', '');
+      return;
+    }
+
+    if (isNone(_graph) && typeOf(_selection) === 'function') {
+      defineProperty(this, 'graph', oneWay('_selection'));
+      return;
+    }
+  },
+
   didInsertElement() {
     this._super(...arguments);
-    Ember.run.scheduleOnce('afterRender', this, 'renderChart');
+
+    let selection = this.get('selection');
+    if (isNone(selection)) {
+      Ember.run.scheduleOnce('afterRender', this, 'renderChart');
+    }
   },
 
   renderChart() {
@@ -15,5 +45,5 @@ export default Ember.Component.extend({
     this.set('selection', select(el));
   }
 }).reopenClass({
-  positionalParams: ['graph']
+  positionalParams: ['_selection', '_graph']
 });
--- tests/integration/components/d3-graph-test.js
+++ tests/integration/components/d3-graph-test.js
@@ -1,24 +1,19 @@
-// import { moduleForComponent, test } from 'ember-qunit';
-// import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
 
-// moduleForComponent('d3-graph', 'Integration | Component | d3 graph', {
-//   integration: true
-// });
+moduleForComponent('d3-graph', 'Integration | Component | d3 graph', {
+  integration: true
+});
 
-// test('it renders', function(assert) {
-//   // Set any properties with this.set('myProperty', 'value');
-//   // Handle any actions with this.on('myAction', function(val) { ... });
+test('it allows to render multiple nested graphs', function(assert) {
+  this.render(hbs`
+    {{#d3-graph as |selection|}}
+      {{d3-graph selection (pipe (d3-append "rect") (d3-attr "name" "foo"))}}
+      {{d3-graph selection (pipe (d3-append "rect") (d3-attr "name" "bar"))}}      
+    {{/d3-graph}}
+  `);
 
-//   this.render(hbs`{{d3-graph}}`);
-
-//   assert.equal(this.$().text().trim(), '');
-
-//   // Template block usage:
-//   this.render(hbs`
-//     {{#d3-graph}}
-//       template block text
-//     {{/d3-graph}}
-//   `);
-
-//   assert.equal(this.$().text().trim(), 'template block text');
-// });
+  assert.ok(this.$('g').length, 1, "only one g is rendered");
+  assert.ok(this.$('rect[name=foo]').length, 1, "rect name foo is rendered");
+  assert.ok(this.$('rect[name=bar]').length, 1, "rect name bar is rendered");  
+});
