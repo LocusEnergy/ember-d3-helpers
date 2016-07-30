447abba5ed786f2433ec1f4e707d1928572e1246
Taras Mankovski <tarasm@gmail.com>
2016-07-30 05:46:04 +0530

    Nested graphs receive result of piped selection

--- tests/integration/components/d3-graph-test.js
+++ tests/integration/components/d3-graph-test.js
@@ -17,3 +17,22 @@ test('it allows to render multiple nested graphs', function(assert) {
   assert.ok(this.$('rect[name=foo]').length, 1, "rect name foo is rendered");
   assert.ok(this.$('rect[name=bar]').length, 1, "rect name bar is rendered");  
 });
+
+test('passing graph pipe yields piped selection', function(assert){
+
+  this.render(hbs`
+    {{#d3-graph (pipe 
+      (d3-append "rect")
+      (d3-attr "class" "piped")
+    ) as |d3|}}
+      {{d3.graph (pipe
+        (d3-append "rect")
+        (d3-attr "class" "deeply-nested")
+      )}}
+    {{/d3-graph}}
+  `);
+
+  assert.ok(this.$('rect.piped').length, "rect with class piped is rendered");
+  assert.ok(this.$('rect.piped > rect.deeply-nested').length, "rect.deeply-nested in piped");
+
+});
