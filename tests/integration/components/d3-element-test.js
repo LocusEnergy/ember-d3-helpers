76157c53419f502d7dc21e7d76da8163978b2a2c
zigahertz <zach.zibrat@locusenergy.com>
2016-09-08 12:35:22 -0700

    adds test

--- tests/integration/components/d3-element-test.js
+++ tests/integration/components/d3-element-test.js
@@ -5,20 +5,15 @@ moduleForComponent('d3-element', 'Integration | Component | d3 element', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  this.set('data', [ { q: 1 }, { q: 2 }, { q: 3}])
-
+test('uses d3-graph to render elements', function(assert) {
+  this.set('data', [ { q: 1 }, { q: 2 }, { q: 3 }])
   this.render(hbs`
     {{d3-element
       element-name='circle'
       selector='my_data'
       data=data
-      on-enter=(pipe
-        (d3-attr 'r' (r/get 'q'))
-      )
     }}
   `);
 
-  assert.equal(this.$('circle').length, 3, 'renders data');
+  assert.equal(this.$('circle').length, 3, 'renders data-joined elements');
 });
