96c7989bb7095c87c9d06d207f949311b6f3458c
zigahertz <zach.zibrat@locusenergy.com>
2016-09-22 18:52:03 -0700

    updates readme + tests

--- addon/components/d3-element.js
+++ addon/components/d3-element.js
@@ -4,5 +4,8 @@ import layout from '../templates/components/d3-element';
 export default Ember.Component.extend({
   layout,
   tagName: '',
-  'with-transition': true
+  'with-transition': true,
+  
+  // default selector; usually developer overwritten
+  selector: '__d3-element'
 });
--- tests/integration/components/d3-element-test.js
+++ tests/integration/components/d3-element-test.js
@@ -7,27 +7,51 @@ moduleForComponent('d3-element', 'Integration | Component | d3 element', {
 
 test('uses d3-graph to render elements', function(assert) {
   this.set('data', [ { r: 2 }, { r: 3 }, { r: 5 }]);
+  this.on('halver', ({ r }) => r / 2);
 
-  this.on('enter-transition', function(a) {
-    console.log('spy')
-    console.log(a)
-  })
   this.render(hbs`
     {{d3-element
       element-name='circle'
-      selector='my_data'
       data=data
       with-transition=false
       enter-transition=(pipe
         (d3-attr 'r' (r/get 'r'))
-        (action 'enter-transition') 
+        (d3-attr 'opacity' (action 'halver'))
       )
     }}
   `);
 
-  let radii = this.$('circle').toArray().map(c => this.$(c).attr('r'))
+  let circles = this.$('circle').toArray();
+  let radii = circles.map(c => this.$(c).attr('r'));
+  let opacities = circles.map(c => this.$(c).attr('opacity'));
 
-  console.log(radii)
+  assert.equal(circles.length, 3, 'renders data-joined elements');
+  assert.deepEqual(radii, ['2', '3', '5'], 'transition events are customizable');
+  assert.deepEqual(opacities, ['1', '1.5', '2.5'], 'transition events are chainable');
+});
 
-  assert.equal(this.$('circle').length, 3, 'renders data-joined elements');
+test('transition event pipes', function(assert) {
+  let steps = [];
+  this.set('data', []);
+  
+  this.on('record-step', (s, selection) => {
+    steps.push(s);
+    return selection;
+  });
+
+  this.render(hbs`
+    {{d3-element
+      element-name='circle'
+      data=data
+      with-transition=false
+      enter-transition=(action 'record-step' 'enter-transition') 
+      on-enter=(action 'record-step' 'on-enter')
+      update-transition=(action 'record-step' 'update-transition')
+      on-update=(action 'record-step' 'on-update')
+      exit-transition=(action 'record-step' 'exit-transition')
+      on-exit=(action 'record-step' 'on-exit')
+    }}
+  `);
+
+  assert.deepEqual(steps, ['enter-transition', 'on-enter', 'update-transition', 'on-update', 'exit-transition', 'on-exit'], 'each transition pipe is called in order');
 });
